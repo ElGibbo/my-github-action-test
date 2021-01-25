8146cbae0c5d7481c6b9699c9eda5c88e031737c
Steve Gibbins <steve.gibbins@responsetap.com>
2021-01-25 14:42:29 +0000

    test: docker action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-set -e
+#!/bin/sh -l
 
-api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
-echo $api_url
-
-pokemon_name=$(curl "${api_url}" | jq ".name")
-echo $pokemon_name
-
-echo "::set-output name=pokemon_name::$pokemon_name"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
